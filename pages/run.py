dcd92cd58c0636a815dc71740ad1316431776031
michalmar <michal.marusan@gmail.com>
2024-06-19 13:43:48 +0000

    inspired by gh issue

--- pages/run.py
+++ pages/run.py
@@ -1,4 +1,5 @@
 import streamlit as st    
+import autogen
 from autogen.agentchat import UserProxyAgent, AssistantAgent, GroupChat, GroupChatManager  
 from autogen.oai.openai_utils import config_list_from_json  
 import warnings  
@@ -6,15 +7,16 @@ import warnings
   
 warnings.filterwarnings('ignore')  
 
-def example_callback(message, sender, recipient):
-        # Example callback functionality
-        print(f"Callback called with message: {message}, from: {sender}, to: {recipient}")
-        with st.expander(f"From {sender} to {recipient}", expanded=False):
-            # st.write(f"From {sender} to {recipient}: ")
-            st.write(message)
-        # with st.chat_message(sender):
-        #     st.write(f"From {sender} to {recipient}: ")
-        #     st.write(message)
+# Callback function to print messages
+# based on https://github.com/microsoft/autogen/issues/478
+def print_messages_callback(recipient, messages, sender, config):     
+    message = messages[-1]
+
+    with st.expander(f"From {message['name']} to {recipient.name}", expanded=False):
+        # st.write(f"From {sender} to {recipient}: ")
+        st.write(message["content"])
+
+    return False, None  # required to ensure the agent communication flow continues
 
 
 class TrackableAssistantAgent(AssistantAgent):
@@ -109,16 +111,21 @@ def config(allowed_transitions, max_round=30, speaker_transitions_type="allowed"
     for agent in st.session_state.saved_agents:
         if agent["type"] == "UserProxyAgent":
             if agent["human_input_mode"] == "ALWAYS":
-                _a = TrackableUserProxyAgent(
+                _a = UserProxyAgent(
                     name=agent["name"],
                     system_message=agent["system_message"],
                     description=agent["description"],
                     human_input_mode=agent["human_input_mode"],
                     code_execution_config=False,
-                    is_termination_msg=lambda msg: "terminate" in msg.get("content").lower(), # TODO: what is this?
+                    is_termination_msg=lambda msg: "terminate" in msg.get("content").lower(),
                 )
+                _a.register_reply(
+                    [autogen.Agent, None],
+                    reply_func=print_messages_callback
+                )
+                _a.get_human_input = lambda prompt: "exit" # TODO: this is a hack to always exit the conversation
             elif agent["human_input_mode"] == "NEVER":
-                _a = TrackableUserProxyAgent(
+                _a = UserProxyAgent(
                     name=agent["name"],
                     system_message=agent["system_message"],
                     description=agent["description"],
@@ -129,14 +136,22 @@ def config(allowed_transitions, max_round=30, speaker_transitions_type="allowed"
                         "use_docker": True,  
                     }
                 )
+                _a.register_reply(
+                    [autogen.Agent, None],
+                    reply_func=print_messages_callback
+                )
         elif agent["type"] == "AssistantAgent":
-            _a = TrackableAssistantAgent(
+            _a = AssistantAgent(
                 name=agent["name"],
                 system_message=agent["system_message"],
                 description=agent["description"],
                 llm_config=gpt4_config
             )
-            _a.callback = example_callback
+            _a.register_reply(
+                [autogen.Agent, None],
+                reply_func=print_messages_callback
+            )
+            
         else:
             st.error(f"Unknown agent type: {agent['type']}")
             st.stop()
@@ -178,15 +193,15 @@ def run(manager, user_proxy, task):
     st.session_state.running = True
     
     if st.session_state.first_query: 
-        st.info("First query...")
         chat_result = user_proxy.initiate_chat(manager, message=task, clear_history=True)  
         st.session_state.first_query = False  
     else:  
         chat_result = user_proxy.initiate_chat(manager, message=task, clear_history=False)  
 
     st.session_state.messages.append(chat_result)  
-    st.write("Chat Summary:")  
-    st.write(chat_result.summary)
+    # st.write("Chat Summary:")  
+    with st.expander("Chat Summary", expanded=True):
+        st.write(chat_result.summary)
     with st.expander("Chat History", expanded=False):
         st.write(chat_result.chat_history)
 
