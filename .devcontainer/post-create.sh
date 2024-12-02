46deaa99b8d7c2e1fafc09adeda0380185f60d6a
Dominique Broeglin <dominique.broeglin@microsoft.com>
2024-12-02 17:22:22 +0000

    Adding Playwright installation to DevContainer setup

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -6,3 +6,8 @@ pip install --upgrade pip
 pip install -r src/requirements.txt
 pip install playwright
 playwright install --with-deps chromium
+
+cd src 
+git clone --depth 1 https://github.com/microsoft/autogen.git 
+cd autogen/python/packages/autogen-magentic-one
+pip install -e .
